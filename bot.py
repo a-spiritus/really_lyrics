8c5a56cc3b720c778d05489d1c7a21b41ee855c1
Alter Spiritus <alterspiritus@gmail.com>
2018-02-28 23:18:13 +0500

    Added nice search on spotify

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ if "HEROKU" in list(os.environ.keys()):
         bot.remove_webhook()
         bot.set_webhook(url="https://liricsreally.herokuapp.com/") # этот url нужно заменить на url вашего Хероку приложения
         return "?", 200
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 80)))
+    server.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT', 80)))
 else:
     # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
     # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
